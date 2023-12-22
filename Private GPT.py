8a22ba052a84dbb56ad9ce8c5c132ddf0669899c
Meet Vaghasiya <meet0209vaghasiya@gmail.com>
2023-12-22 18:26:21 +0530

    Add files via upload

---
+++ Private GPT.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import google.generativeai as genai
+
+genai.configure(api_key="GOOGLE_API_KEY")
+
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+def get_gemini_response(question):
+    
+    response =chat.send_message(question,stream=True)
+    return response
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Private GPT")
+
+input=st.text_input("Input: ",key="input")
+
+
+submit=st.button("Ask the question")
+
+## If ask button is clicked
+
+if submit:
+    
+    response=get_gemini_response(input)
+    st.subheader("The Response is")
+    for chunk in response:
+        print(st.write(chunk.text))
+        print("_"*80)
